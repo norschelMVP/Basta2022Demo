d2342216e8c1e9e511d26648a1d7f7c4779631e4
Nico Orschel <nico.orschel@aitgmbh.de>
2022-02-24 08:29:34 +0100

    Added version label

--- WinNetCoreApp/WinNetCoreApp/MainWindow.xaml.cs
+++ WinNetCoreApp/WinNetCoreApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,7 @@ namespace WinNetCoreApp
         public MainWindow()
         {
             InitializeComponent();
+            lblVersion.Content = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
     }
 }
